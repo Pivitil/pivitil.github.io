cb9f502e307ce148d7ae425b1b9642a3ce6a0771
Jason Unger <jason_unger@outlook.com>
2024-12-31 14:28:23 -0800

    Updated exercises for Legs, Chest, Back, and Arms

--- src/scripts/data/Exercises.js
+++ src/scripts/data/Exercises.js
@@ -1,7 +1,7 @@
 export const exercises = {
-    Chest: ['Bench Press', 'Chest Fly', 'Push Up'],
-    Back: ['Pull Up', 'Deadlift', 'Row'],
-    Legs: ['Squat', 'Leg Press', 'Lunge'],
-    Arms: ['Bicep Curl', 'Tricep Extension', 'Hammer Curl'],
+    Legs: ['Adductors (Warm Up)', 'Leg Press', 'Single Leg Extension', 'Hamstring Curls', 'Adductors (Run the Rack)'],
+    Chest: ['Pec Deck (Warm Up)', 'Chest Press', 'Plate Loaded Chest Fly', 'Pec Deck (Run the Rack)'],
+    Back: ['Pull Overs (Warm Up)', 'Row Machine', 'Single Arm Lat Pull Down', 'Pull Overs (Run the Rack)'],
+    Arms: ['Close Grip Bench', 'Cable Push Down', 'Dumbbell Curls', 'Preacher Curls'],
     Shoulders: ['Shoulder Press', 'Lateral Raise', 'Front Raise']
 };
